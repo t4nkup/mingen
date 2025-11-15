c3f1db39eb15bb3694ca628673102feb161e2855
t4nkup <t4nkup@gmail.com>
2025-11-15 10:58:17 -1000

    #

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    const Color darkGreen = {20, 160, 133, 255};
+    const Color darkGreen = { 20, 160, 133, 255 };
     
     const int screenWidth = 800;
     const int screenHeight = 600;
