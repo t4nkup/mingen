0da7d9322f7c69210c8ae46be0abf90bb5438b36
t4nkup <t4nkup@gmail.com>
2025-11-15 15:41:07 -1000

    #

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
-#include "../lib/raylib/raylib.h"
+#include "../lib/raylib.h"
 #include "../include/ball.h"
 
 int main(void)
 {
-    const Color darkGreen = { 20, 160, 133, 255 };
+    Color darkGreen = { 20, 160, 133, 255 };
     
-    const int screenWidth = 800;
-    const int screenHeight = 600;
+    int screenWidth = 800;
+    int screenHeight = 600;
     
     Ball ball;
     Ball_Init(&ball); // initialize the ball
